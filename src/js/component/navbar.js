61f97d39a553bdae06be36c4d95d32bd6e58e984
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-03-27 19:58:29 +0000

    creando vista detalles

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,7 +6,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3 py-4">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1 ms-5"><img className="logostarwars" src="https://logos-marcas.com/wp-content/uploads/2020/11/Star-Wars-Logo.png" alt=""/></span>
+				<span className="navbar-brand mb-0 h1 ms-5"><img className="logostarwars" src="https://1000logos.net/wp-content/uploads/2017/06/Star-Wars-Logo-1.png" alt=""/></span>
 			</Link>
 			<div className="ml-auto">
 				{/* <Link to="/demo"> */}
@@ -14,7 +14,7 @@ export const Navbar = () => {
 					<!-- Example single danger button --> */}
 					<div className="btn-group me-5">
 						<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-							Favorites
+							Favorites<span className="badge bg-secondary mx-1">4</span>
 						</button>
 						<ul className="dropdown-menu">
 							<li><a className="dropdown-item" href="#">Action</a></li>
--- src/js/views/detalles.js
+++ src/js/views/detalles.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Context } from "../store/appContext.js";
 import { useParams } from "react-router";
-
+import "../../styles/detalles.css";
 // import "../../styles/detalles.css";
 
 
@@ -19,7 +19,24 @@ export const Detalles = () => {
 
     return (
         <>
-        <h1>Detalles</h1>
+            <div className="jumbotron mx-5">
+                <div className="tittle-top d-flex">
+                <img src="https://productroulette.com/_nuxt/img/blog1.d1e9eb0.jpg" className="img-left" alt="..." />
+                <h1 className="display-5">Hello, world!</h1>
+                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr className="my-4" />
+                </div>
+            <div>
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <p className="lead">
+                    <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                </p>
+            </div>
+            </div>
+            {/* <div className="detalles container d-flex justify-content-center">
+        <img src="https://productroulette.com/_nuxt/img/blog1.d1e9eb0.jpg" className="img-left" alt="..."/>
+        <h1 className="tittle">Nombre Personaje</h1>
+        </div> */}
         </>
     )
 
